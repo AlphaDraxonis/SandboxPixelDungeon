df5f997a2c643233369788776dc40e9a8aa09a84
AlphaDraxonis <alphadraxonis@gmail.com>
2023-04-17 19:49:14 +0200

    New Class

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/editor/CustomTestLevel.java
@@ -0,0 +1,19 @@
+package com.shatteredpixel.shatteredpixeldungeon.levels.editor;
+
+import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
+
+public class CustomTestLevel extends Level {
+    @Override
+    protected boolean build() {
+        return false;
+    }
+
+    @Override
+    protected void createMobs() {
+
+    }
+
+    @Override
+    protected void createItems() {
+    }
+}
