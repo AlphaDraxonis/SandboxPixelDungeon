a1359257e54f6f3411e3b5ec240095046b19055d
AlphaDraxonis <alphadraxonis@gmail.com>
2023-06-06 17:51:13 +0200

    v0.3: made draggclicking select all cells on the path

--- core/src/main/java/com/alphadraxonis/sandboxpixeldungeon/editor/scene/EditorCellSelector.java
+++ core/src/main/java/com/alphadraxonis/sandboxpixeldungeon/editor/scene/EditorCellSelector.java
@@ -4,6 +4,8 @@ import com.alphadraxonis.sandboxpixeldungeon.editor.EditorScene;
 import com.alphadraxonis.sandboxpixeldungeon.scenes.CellSelector;
 import com.alphadraxonis.sandboxpixeldungeon.tiles.DungeonTilemap;
 import com.watabou.input.PointerEvent;
+import com.watabou.utils.PathFinder;
+import com.watabou.utils.PointF;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -59,10 +61,14 @@ public class EditorCellSelector extends CellSelector {
     }
 
     private final Set<Integer> lastSelectedCells = new HashSet<>();
+    private int lastCell = -1;
+    private PointF lastPoint;
 
     @Override
     protected void onPointerUp(PointerEvent event) {
         lastSelectedCells.clear();
+        lastCell = -1;
+        lastPoint = null;
         super.onPointerUp(event);
     }
 
@@ -72,9 +78,37 @@ public class EditorCellSelector extends CellSelector {
                 (int) event.current.x,
                 (int) event.current.y,
                 true);
-        if (lastSelectedCells.contains(cell) ||EditorScene.customLevel().isBorder(cell)) return; //dont trigger every frame, only when a new cell was entered
+        if (lastSelectedCells.contains(cell) || EditorScene.customLevel().isBorder(cell))
+            return; //dont trigger every frame, only when a new cell was entered
         lastSelectedCells.add(cell);
+        if (lastPoint != null)
+            checkForMissingCells(cell, lastCell, event.current, lastPoint, event.button);
+        lastPoint = event.current;
+        lastCell = cell;
         select(cell, event.button);
+    }
+
+    //if pointer is faster than the fps, some cells are skipped, this corrects it by drawing straight lines between the known points
+    private void checkForMissingCells(int cell, int lastCell, PointF now, PointF was, int button) {
+
+        for (int i : PathFinder.NEIGHBOURS9) {
+            if (cell + i == lastCell) return;
+        }
+
+        PointF middle = new PointF(now.x + (was.x - now.x) / 2, now.y + (was.y - now.y) / 2);
+
+        int middleCell = ((DungeonTilemap) target).screenToTile(
+                (int) middle.x,
+                (int) middle.y,
+                true);
+
+        checkForMissingCells(middleCell, cell, middle, now, button);//Recursively go from the middle to the borders
+        checkForMissingCells(middleCell, lastCell, middle, was, button);
+
+        if (lastSelectedCells.contains(middleCell) || EditorScene.customLevel().isBorder(middleCell))
+            return;
+        lastSelectedCells.add(middleCell);
+        select(middleCell, button);
     }
 
     @Override
