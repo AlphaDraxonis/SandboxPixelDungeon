60c67bae4c166bf817c44704627501beedcc7d78
Evan Debenham <evan@shatteredpixel.com>
2023-07-03 20:19:30 +0200

    v2.1.4: added a null safety check in InventoryPaneSlot

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/InventoryPane.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/InventoryPane.java
@@ -572,7 +572,7 @@ public class InventoryPane extends Component {
 
             if (selector == null && item.defaultAction() != null) {
                 item.execute(Dungeon.hero);
-                if (item.usesTargeting) {
+                if (item != null && item.usesTargeting) {
                     targetingSlot = this;
                     InventoryPane.useTargeting();
                 }
