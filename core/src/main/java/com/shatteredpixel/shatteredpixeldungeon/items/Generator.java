d145a040891b261031dc37d73cbceea5cc78d749
Evan Debenham <Evan@ShatteredPixel.com>
2023-07-03 18:11:43 -0400

    v2.1.4: fixed randomWeapon not always using defaults when specified
    (cherry picked from commit c8ebdde5e7cf3a191a56fa922497e42a50d53020)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Generator.java
@@ -647,7 +647,7 @@ public class Generator {
 	}
 
 	public static MeleeWeapon randomWeapon(boolean useDefaults) {
-		return randomWeapon(Dungeon.depth / 5, false);
+		return randomWeapon(Dungeon.depth / 5, useDefaults);
 	}
 	
 	public static MeleeWeapon randomWeapon(int floorSet, boolean useDefaults) {
