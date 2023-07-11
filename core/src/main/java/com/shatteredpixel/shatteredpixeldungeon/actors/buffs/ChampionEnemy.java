4a6c13d6dc8b36e02d3bfd3f76f73dd5d17edce0
Evan Debenham <evan@shatteredpixel.com>
2023-07-11 18:39:10 +0200

    v2.2.0: balance changes to hostile champions

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/ChampionEnemy.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/ChampionEnemy.java
@@ -40,248 +40,249 @@ import com.watabou.utils.Random;
 
 public abstract class ChampionEnemy extends Buff {
 
-    {
-        type = buffType.POSITIVE;
-    }
+	{
+		type = buffType.POSITIVE;
+	}
 
-    protected int color;
+	protected int color;
 
-    @Override
-    public int icon() {
-        return BuffIndicator.CORRUPT;
-    }
+	@Override
+	public int icon() {
+		return BuffIndicator.CORRUPT;
+	}
 
-    @Override
-    public void tintIcon(Image icon) {
-        icon.hardlight(color);
-    }
+	@Override
+	public void tintIcon(Image icon) {
+		icon.hardlight(color);
+	}
 
-    @Override
-    public void fx(boolean on) {
-        if (on) target.sprite.aura(color);
-        else target.sprite.clearAura();
-    }
+	@Override
+	public void fx(boolean on) {
+		if (on) target.sprite.aura( color );
+		else target.sprite.clearAura();
+	}
 
-    public void onAttackProc(Char enemy) {
+	public void onAttackProc(Char enemy ){
 
-    }
+	}
 
-    public boolean canAttackWithExtraReach(Char enemy) {
-        return false;
-    }
+	public boolean canAttackWithExtraReach( Char enemy ){
+		return false;
+	}
 
-    public float meleeDamageFactor() {
-        return 1f;
-    }
+	public float meleeDamageFactor(){
+		return 1f;
+	}
 
-    public float damageTakenFactor() {
-        return 1f;
-    }
+	public float damageTakenFactor(){
+		return 1f;
+	}
 
-    public float evasionAndAccuracyFactor() {
-        return 1f;
-    }
+	public float evasionAndAccuracyFactor(){
+		return 1f;
+	}
 
-    {
-        immunities.add(AllyBuff.class);
-    }
+	{
+		immunities.add(AllyBuff.class);
+	}
 
-    public static void rollForChampion(Mob m) {
-        if (Dungeon.mobsToChampion <= 0) Dungeon.mobsToChampion = 8;
+	public static void rollForChampion(Mob m){
+		if (Dungeon.mobsToChampion <= 0) Dungeon.mobsToChampion = 8;
 
-        Dungeon.mobsToChampion--;
+		Dungeon.mobsToChampion--;
 
-        //we roll for a champion enemy even if we aren't spawning one to ensure that
-        //mobsToChampion does not affect levelgen RNG (number of calls to Random.Int() is constant)
-        Class<? extends ChampionEnemy> buffCls;
-        switch (Random.Int(6)) {
-            case 0:
-            default:
-                buffCls = Blazing.class;
-                break;
-            case 1:
-                buffCls = Projecting.class;
-                break;
-            case 2:
-                buffCls = AntiMagic.class;
-                break;
-            case 3:
-                buffCls = Giant.class;
-                break;
-            case 4:
-                buffCls = Blessed.class;
-                break;
-            case 5:
-                buffCls = Growing.class;
-                break;
-        }
+		//we roll for a champion enemy even if we aren't spawning one to ensure that
+		//mobsToChampion does not affect levelgen RNG (number of calls to Random.Int() is constant)
+		Class<?extends ChampionEnemy> buffCls;
+		switch (Random.Int(6)){
+			case 0: default:    buffCls = Blazing.class;      break;
+			case 1:             buffCls = Projecting.class;   break;
+			case 2:             buffCls = AntiMagic.class;    break;
+			case 3:             buffCls = Giant.class;        break;
+			case 4:             buffCls = Blessed.class;      break;
+			case 5:             buffCls = Growing.class;      break;
+		}
 
-        if (Dungeon.mobsToChampion <= 0 && Dungeon.isChallenged(Challenges.CHAMPION_ENEMIES)) {
-            Buff.affect(m, buffCls);
-            m.state = m.WANDERING;
-        }
-    }
+		if (Dungeon.mobsToChampion <= 0 && Dungeon.isChallenged(Challenges.CHAMPION_ENEMIES)) {
+			Buff.affect(m, buffCls);
+			m.state = m.WANDERING;
+		}
+	}
 
-    public static class Blazing extends ChampionEnemy {
+	public static class Blazing extends ChampionEnemy {
 
-        {
-            color = 0xFF8800;
-        }
+		{
+			color = 0xFF8800;
+		}
 
-        @Override
-        public void onAttackProc(Char enemy) {
-            Buff.affect(enemy, Burning.class).reignite(enemy);
-        }
+		@Override
+		public void onAttackProc(Char enemy) {
+			if (!Dungeon.level.water[enemy.pos]) {
+				Buff.affect(enemy, Burning.class).reignite(enemy);
+			}
+		}
 
-        @Override
-        public void detach() {
-            //don't trigger when killed by being knocked into a pit
-            if (target.pos > 0  && !CustomDungeon.isEditing()) {
-                if (target.flying || !Dungeon.level.pit[target.pos]) {
-                    for (int i : PathFinder.NEIGHBOURS9) {
-                        if (!Dungeon.level.solid[target.pos + i]) {
-                            GameScene.add(Blob.seed(target.pos + i, 2, Fire.class));//FIXME this method added Permafire on the map when playing, maybe add blobs to inv as well?
-                        }
-                    }
-                }
-            }
-            super.detach();
-        }
+		@Override
+		public void detach() {
+			//don't trigger when killed by being knocked into a pit
+			if (target.pos > 0  && !CustomDungeon.isEditing()) {
+				if (target.flying || !Dungeon.level.pit[target.pos]) {
+					for (int i : PathFinder.NEIGHBOURS9) {
+						if (!Dungeon.level.solid[target.pos + i] && !Dungeon.level.water[target.pos + i]) {
+							GameScene.add(Blob.seed(target.pos + i, 2, Fire.class));
+						}
+					}
+				}
+			}
+			super.detach();
+		}
 
-        @Override
-        public float meleeDamageFactor() {
-            return 1.25f;
-        }
+		@Override
+		public float meleeDamageFactor() {
+			return 1.25f;
+		}
 
-        {
-            immunities.add(Burning.class);
-        }
-    }
+		{
+			immunities.add(Burning.class);
+		}
+	}
 
-    public static class Projecting extends ChampionEnemy {
+	public static class Projecting extends ChampionEnemy {
 
-        {
-            color = 0x8800FF;
-        }
+		{
+			color = 0x8800FF;
+		}
 
-        @Override
-        public float meleeDamageFactor() {
-            return 1.25f;
-        }
+		@Override
+		public float meleeDamageFactor() {
+			return 1.25f;
+		}
 
-        @Override
-        public boolean canAttackWithExtraReach(Char enemy) {
-            return target.fieldOfView[enemy.pos]; //if it can see it, it can attack it.
-        }
-    }
+		@Override
+		public boolean canAttackWithExtraReach(Char enemy) {
+			if (Dungeon.level.distance( target.pos, enemy.pos ) > 5){
+				return false;
+			} else {
+				boolean[] passable = BArray.not(Dungeon.level.solid, null);
+				for (Char ch : Actor.chars()) {
+					//our own tile is always passable
+					passable[ch.pos] = ch == target;
+				}
 
-    public static class AntiMagic extends ChampionEnemy {
+				PathFinder.buildDistanceMap(enemy.pos, passable, 5);
 
-        {
-            color = 0x00FF00;
-        }
+				return PathFinder.distance[target.pos] <= 5;
+			}
+		}
+	}
 
-        @Override
-        public float damageTakenFactor() {
-            return 0.75f;
-        }
+	public static class AntiMagic extends ChampionEnemy {
 
-        {
-            immunities.addAll(com.shatteredpixel.shatteredpixeldungeon.items.armor.glyphs.AntiMagic.RESISTS);
-        }
+		{
+			color = 0x00FF00;
+		}
 
-    }
+		@Override
+		public float damageTakenFactor() {
+			return 0.5f;
+		}
 
-    //Also makes target large, see Char.properties()
-    public static class Giant extends ChampionEnemy {
+		{
+			immunities.addAll(com.shatteredpixel.shatteredpixeldungeon.items.armor.glyphs.AntiMagic.RESISTS);
+		}
 
-        {
-            color = 0x0088FF;
-        }
+	}
 
-        @Override
-        public float damageTakenFactor() {
-            return 0.25f;
-        }
+	//Also makes target large, see Char.properties()
+	public static class Giant extends ChampionEnemy {
 
-        @Override
-        public boolean canAttackWithExtraReach(Char enemy) {
-            if (Dungeon.level.distance(target.pos, enemy.pos) > 2) {
-                return false;
-            } else {
-                boolean[] passable = BArray.not(Dungeon.level.solid, null);
-                for (Char ch : Actor.chars()) {
-                    //our own tile is always passable
-                    passable[ch.pos] = ch == target;
-                }
+		{
+			color = 0x0088FF;
+		}
 
-                PathFinder.buildDistanceMap(enemy.pos, passable, 2);
+		@Override
+		public float damageTakenFactor() {
+			return 0.2f;
+		}
 
-                return PathFinder.distance[target.pos] <= 2;
-            }
-        }
-    }
+		@Override
+		public boolean canAttackWithExtraReach(Char enemy) {
+			if (Dungeon.level.distance( target.pos, enemy.pos ) > 2){
+				return false;
+			} else {
+				boolean[] passable = BArray.not(Dungeon.level.solid, null);
+				for (Char ch : Actor.chars()) {
+					//our own tile is always passable
+					passable[ch.pos] = ch == target;
+				}
 
-    public static class Blessed extends ChampionEnemy {
+				PathFinder.buildDistanceMap(enemy.pos, passable, 2);
 
-        {
-            color = 0xFFFF00;
-        }
+				return PathFinder.distance[target.pos] <= 2;
+			}
+		}
+	}
 
-        @Override
-        public float evasionAndAccuracyFactor() {
-            return 3f;
-        }
-    }
+	public static class Blessed extends ChampionEnemy {
 
-    public static class Growing extends ChampionEnemy {
+		{
+			color = 0xFFFF00;
+		}
 
-        {
-            color = 0xFF0000;
-        }
+		@Override
+		public float evasionAndAccuracyFactor() {
+			return 4f;
+		}
+	}
 
-        private float multiplier = 1.19f;
+	public static class Growing extends ChampionEnemy {
 
-        @Override
-        public boolean act() {
-            multiplier += 0.01f;
-            spend(3 * TICK);
-            return true;
-        }
+		{
+			color = 0xFF0000;
+		}
 
-        @Override
-        public float meleeDamageFactor() {
-            return multiplier;
-        }
+		private float multiplier = 1.19f;
 
-        @Override
-        public float damageTakenFactor() {
-            return 1f / multiplier;
-        }
+		@Override
+		public boolean act() {
+			multiplier += 0.01f;
+			spend(4*TICK);
+			return true;
+		}
 
-        @Override
-        public float evasionAndAccuracyFactor() {
-            return multiplier;
-        }
+		@Override
+		public float meleeDamageFactor() {
+			return multiplier;
+		}
 
-        @Override
-        public String desc() {
-            return Messages.get(this, "desc", (int) (100 * (multiplier - 1)), (int) (100 * (1 - 1f / multiplier)));
-        }
+		@Override
+		public float damageTakenFactor() {
+			return 1f/multiplier;
+		}
 
-        private static final String MULTIPLIER = "multiplier";
+		@Override
+		public float evasionAndAccuracyFactor() {
+			return multiplier;
+		}
 
-        @Override
-        public void storeInBundle(Bundle bundle) {
-            super.storeInBundle(bundle);
-            bundle.put(MULTIPLIER, multiplier);
-        }
+		@Override
+		public String desc() {
+			return Messages.get(this, "desc", (int)(100*(multiplier-1)), (int)(100*(1 - 1f/multiplier)));
+		}
 
-        @Override
-        public void restoreFromBundle(Bundle bundle) {
-            super.restoreFromBundle(bundle);
-            multiplier = bundle.getFloat(MULTIPLIER);
-        }
-    }
+		private static final String MULTIPLIER = "multiplier";
+
+		@Override
+		public void storeInBundle(Bundle bundle) {
+			super.storeInBundle(bundle);
+			bundle.put(MULTIPLIER, multiplier);
+		}
+
+		@Override
+		public void restoreFromBundle(Bundle bundle) {
+			super.restoreFromBundle(bundle);
+			multiplier = bundle.getFloat(MULTIPLIER);
+		}
+	}
 
 }
