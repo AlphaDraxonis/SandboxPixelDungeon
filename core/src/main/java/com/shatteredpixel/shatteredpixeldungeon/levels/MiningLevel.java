cbf37766c74373a5aadb9434ac04e5ff801bc342
Evan Debenham <evan@shatteredpixel.com>
2023-07-13 21:05:17 +0200

    v2.2.0: improved border visuals for mining level

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/MiningLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/MiningLevel.java
@@ -38,14 +38,16 @@ import com.watabou.noosa.Image;
 import com.watabou.noosa.Tilemap;
 import com.watabou.noosa.audio.Music;
 
+import java.util.Arrays;
+
 public class MiningLevel extends Level {
 
-    {
-        color1 = 0x534f3e;
-        color2 = 0xb9d661;
-    }
+	{
+		color1 = 0x534f3e;
+		color2 = 0xb9d661;
+	}
 
-    @Override
+	@Override
 	public void playLevelMusic() {
 		Music.INSTANCE.playTracks(
 				new String[]{Assets.Music.CAVES_1, Assets.Music.CAVES_2, Assets.Music.CAVES_2},
@@ -54,26 +56,26 @@ public class MiningLevel extends Level {
 	}
 
 	@Override
-    public String tilesTex() {
-        return Assets.Environment.TILES_CAVES;
-    }
+	public String tilesTex() {
+		return Assets.Environment.TILES_CAVES;
+	}
 
-    @Override
-    public String waterTex() {
-        return Assets.Environment.WATER_CAVES;
-    }
+	@Override
+	public String waterTex() {
+		return Assets.Environment.WATER_CAVES;
+	}
 
-    @Override
-    protected boolean build() {
+	@Override
+	protected boolean build() {
 
-        setSize(32, 32);
+		setSize(32, 32);
 
-        CaveRoom c = new CaveRoom();
-        c.set(1, 1, 31, 31);
-        c.paint(this);
+		CaveRoom c = new CaveRoom();
+		c.set(1, 1, 31, 31);
+		c.paint(this);
 
-        Painter.fill(this, 15, 15, 3, 3, Terrain.EMPTY);
-        int entrance = 16 * width() + 16;
+		Painter.fill(this, 15, 15, 3, 3, Terrain.EMPTY);
+		int entrance = 16 * width() + 16;
 
 //        transitions.add(new LevelTransition(this,
 //                entrance,
@@ -82,95 +84,121 @@ public class MiningLevel extends Level {
 //                0,
 //                LevelTransition.Type.BRANCH_EXIT));
 
-        map[entrance] = Terrain.ENTRANCE;
+		map[entrance] = Terrain.ENTRANCE;
 
-        Painter painter = new CavesPainter()
-                .setWater(0.35f, 6)
-                .setGrass(0.10f, 3);
+		Painter painter = new CavesPainter()
+				.setWater(0.35f, 6)
+				.setGrass(0.10f, 3);
 
-        painter.paint(this, null);
+		painter.paint(this, null);
 
-        BorderDarken vis = new BorderDarken();
+		CustomTilemap vis = new BorderTopDarken();
+		vis.setRect(0, 0, width, 1);
+		customTiles.add(vis);
+
+		vis = new BorderWallsDarken();
 		vis.setRect(0, 0, width, height);
-		customWalls.add(vis);return true;
-    }
+		customWalls.add(vis);
 
-    @Override
-    public Mob createMob() {
-        return null;
-    }
+		return true;
+	}
 
-    @Override
-    protected void createMobs() {
-    }
+	@Override
+	public Mob createMob() {
+		return null;
+	}
 
-    public Actor addRespawner() {
-        return null;
-    }
+	@Override
+	protected void createMobs() {
+	}
 
-    @Override
-    protected void createItems() {
-        Item item = Bones.get();
-        if (item != null) {
-            drop( item, entrance()-width() ).setHauntedIfCursed().type = Heap.Type.REMAINS;
-        }
-    }
+	public Actor addRespawner() {
+		return null;
+	}
 
-    @Override
-    public int randomRespawnCell( Char ch ) {
-        return entrance()-width();
-    }
+	@Override
+	protected void createItems() {
+		Item item = Bones.get();
+		if (item != null) {
+			drop( item, entrance()-width() ).setHauntedIfCursed().type = Heap.Type.REMAINS;
+		}
+	}
 
+	@Override
+	public int randomRespawnCell( Char ch ) {
+		return entrance()-width();
+	}
 
-    @Override
-    public String tileName( int tile ) {
-        switch (tile) {
-            case Terrain.GRASS:
-                return Messages.get(CavesLevel.class, "grass_name");
-            case Terrain.HIGH_GRASS:
-                return Messages.get(CavesLevel.class, "high_grass_name");
-            case Terrain.WATER:
-                return Messages.get(CavesLevel.class, "water_name");
-            case Terrain.STATUE:
-                //city statues are used
-                return Messages.get(CityLevel.class, "statue_name");
-            default:
-                return super.tileName( tile );
-        }
-    }
+	@Override
+	public String tileName( int tile ) {
+		switch (tile) {
+			case Terrain.GRASS:
+				return Messages.get(CavesLevel.class, "grass_name");
+			case Terrain.HIGH_GRASS:
+				return Messages.get(CavesLevel.class, "high_grass_name");
+			case Terrain.WATER:
+				return Messages.get(CavesLevel.class, "water_name");
+			case Terrain.STATUE:
+				//city statues are used
+				return Messages.get(CityLevel.class, "statue_name");
+			default:
+				return super.tileName( tile );
+		}
+	}
 
-    @Override
-    public String tileDesc( int tile, int cell ) {
-        switch (tile) {
-            case Terrain.WATER:
-                return super.tileDesc( tile, cell ) + "\n\n" + Messages.get(CavesBossLevel.class, "water_desc");
-            case Terrain.ENTRANCE:
-                return Messages.get(CavesLevel.class, "entrance_desc")+appendNoTransWarning(cell);
-            case Terrain.EXIT:
-                //city exit is used
-                return Messages.get(CityLevel.class, "exit_desc")+appendNoTransWarning(cell);
-            case Terrain.HIGH_GRASS:
-                return Messages.get(CavesLevel.class, "high_grass_desc");
-            case Terrain.WALL_DECO:
-                return Messages.get(CavesLevel.class, "wall_deco_desc");
-            case Terrain.BOOKSHELF:
-                return Messages.get(CavesLevel.class, "bookshelf_desc");
-            //city statues are used
-            case Terrain.STATUE:
-                return Messages.get(CityLevel.class, "statue_desc");
-            default:
-                return super.tileDesc( tile, cell );
-        }
-    }
+	@Override
+	public String tileDesc( int tile, int cell ) {
+		switch (tile) {
+			case Terrain.WATER:
+				return super.tileDesc( tile, cell ) + "\n\n" + Messages.get(CavesBossLevel.class, "water_desc");
+			case Terrain.ENTRANCE:
+				return Messages.get(CavesLevel.class, "entrance_desc");
+			case Terrain.EXIT:
+				//city exit is used
+				return Messages.get(CityLevel.class, "exit_desc");
+			case Terrain.HIGH_GRASS:
+				return Messages.get(CavesLevel.class, "high_grass_desc");
+			case Terrain.WALL_DECO:
+				return Messages.get(CavesLevel.class, "wall_deco_desc");
+			case Terrain.BOOKSHELF:
+				return Messages.get(CavesLevel.class, "bookshelf_desc");
+			//city statues are used
+			case Terrain.STATUE:
+				return Messages.get(CityLevel.class, "statue_desc");
+			default:
+				return super.tileDesc( tile, cell );
+		}
+	}
 
-    @Override
-    public Group addVisuals() {
-        super.addVisuals();
-        CavesLevel.addCavesVisuals(this, visuals);
-        return visuals;
-    }
+	@Override
+	public Group addVisuals() {
+		super.addVisuals();
+		CavesLevel.addCavesVisuals(this, visuals);
+		return visuals;
+	}
 
-	public static class BorderDarken extends CustomTilemap{
+	public static class BorderTopDarken extends CustomTilemap {
+
+		{
+			texture = Assets.Environment.CAVES_QUEST;
+		}
+
+		@Override
+		public Tilemap create() {
+			Tilemap v = super.create();
+			int[] data = new int[tileW*tileH];
+			Arrays.fill(data, 1);
+			v.map( data, tileW );
+			return v;
+		}
+
+		@Override
+		public Image image(int tileX, int tileY) {
+			return null;
+		}
+	}
+
+	public static class BorderWallsDarken extends CustomTilemap {
 
 		{
 			texture = Assets.Environment.CAVES_QUEST;
@@ -181,12 +209,10 @@ public class MiningLevel extends Level {
 			Tilemap v = super.create();
 			int[] data = new int[tileW*tileH];
 			for (int i = 0; i < data.length; i++){
-				if (i < tileW){
-					data[i] = 2;
-				} else if (i % tileW == 0 || i % tileW == tileW-1){
+				if (i % tileW == 0 || i % tileW == tileW-1){
 					data[i] = 1;
 				} else if (i + 2*tileW > data.length) {
-					data[i] = 3;
+					data[i] = 2;
 				} else {
 					data[i] = -1;
 				}
