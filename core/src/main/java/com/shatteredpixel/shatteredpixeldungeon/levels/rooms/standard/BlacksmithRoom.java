f6868fe85797d783e3298b31bfc37a3d1d19c000
Evan Debenham <evan@shatteredpixel.com>
2023-08-04 18:53:52 +0200

    v2.2.0: added a proper entrance room for the blacksmith quest area

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/BlacksmithRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/BlacksmithRoom.java
@@ -21,6 +21,8 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.levels.rooms.standard;
 
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs.Blacksmith;
 import com.shatteredpixel.shatteredpixeldungeon.items.Generator;
@@ -28,63 +30,113 @@ import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Terrain;
 import com.shatteredpixel.shatteredpixeldungeon.levels.painters.Painter;
 import com.shatteredpixel.shatteredpixeldungeon.levels.traps.BurningTrap;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.tiles.CustomTilemap;
+import com.watabou.noosa.Tilemap;
 import com.watabou.utils.Point;
 import com.watabou.utils.Random;
 
 public class BlacksmithRoom extends StandardRoom {
 
-    private boolean hasBlacksmith;
+	private boolean hasBlacksmith;
+	private int entrancePos;
 
-    @Override
-    public int minWidth() {
-        return Math.max(super.minWidth(), 6);
-    }
+	@Override
+	public int minWidth() {
+		return Math.max(super.minWidth(), 6);
+	}
+	
+	@Override
+	public int minHeight() {
+		return Math.max(super.minHeight(), 6);
+	}
+	
+	public void paint(Level level ) {
 
-    @Override
-    public int minHeight() {
-        return Math.max(super.minHeight(), 6);
-    }
+		Painter.fill( level, this, Terrain.WALL );
+		Painter.fill( level, this, 1, Terrain.TRAP );
+		Painter.fill( level, this, 2, Terrain.EMPTY_SP );
+		
+		for (int i=0; i < 2; i++) {
+			int pos;
+			do {
+				pos = level.pointToCell(random());
+			} while (level.map[pos] != Terrain.EMPTY_SP);
+			level.drop(
+				Generator.random( Random.oneOf(
+					Generator.Category.ARMOR,
+					Generator.Category.WEAPON,
+					Generator.Category.MISSILE
+				) ), pos );
+		}
+		
+		for (Door door : connected.values()) {
+			door.set( Door.Type.REGULAR );
+			Painter.drawInside( level, this, door, 1, Terrain.EMPTY );
+		}
 
-    public void paint(Level level) {
+		Random.pushGenerator(Dungeon.seedCurLevel()+1);
+//			do {
+				entrancePos = level.pointToCell(random( 2 ));
+//			} while (level.heaps.get( npc.pos ) != null);
+		Random.popGenerator();
 
-        Painter.fill(level, this, Terrain.WALL);
-        Painter.fill(level, this, 1, Terrain.TRAP);
-        Painter.fill(level, this, 2, Terrain.EMPTY_SP);
+		QuestEntrance vis = new QuestEntrance();
+		vis.pos(entrancePos, level);
+		level.customTiles.add(vis);
 
-        for (int i = 0; i < 2; i++) {
-            int pos;
-            do {
-                pos = level.pointToCell(random());
-            } while (level.map[pos] != Terrain.EMPTY_SP);
-            level.drop(
-                    Generator.random(Random.oneOf(
-                            Generator.Category.ARMOR,
-                            Generator.Category.WEAPON,
-                            Generator.Category.MISSILE
-                    )), pos);
-        }
+//		level.transitions.add(new LevelTransition(level,
+//				entrancePos,
+//				LevelTransition.Type.BRANCH_EXIT,
+//				Dungeon.depth,
+//				Dungeon.branch+1,
+//				LevelTransition.Type.BRANCH_ENTRANCE));
+		Painter.set(level, entrancePos, Terrain.EXIT);
 
-        for (Door door : connected.values()) {
-            door.set(Door.Type.REGULAR);
-            Painter.drawInside(level, this, door, 1, Terrain.EMPTY);
-        }
+		for(Point p : getPoints()) {
+			int cell = level.pointToCell(p);
+			if (level.map[cell] == Terrain.TRAP){
+				level.setTrap(new BurningTrap().reveal(), cell);
+			}
+		}
+	}
 
-        for (Point p : getPoints()) {
-            int cell = level.pointToCell(p);
-            if (level.map[cell] == Terrain.TRAP) {
-                level.setTrap(new BurningTrap().reveal(), cell);
-            }
-        }
-    }
+	public static class QuestEntrance extends CustomTilemap {
 
-    public boolean placeBlacksmith(Blacksmith blacksmith, Level level) {
-        if (hasBlacksmith) return false;
-        do {
-            blacksmith.pos = level.pointToCell(random(2));
-        } while (Actor.findChar(blacksmith.pos) == null
-                && blacksmith.pos != level.exit()
-                && level.heaps.get(blacksmith.pos) != null);
-        level.mobs.add(blacksmith);
-        return hasBlacksmith = true;
-    }
+		{
+			texture = Assets.Environment.CAVES_QUEST;
+
+			tileW = tileH = 1;
+		}
+
+		@Override
+		public Tilemap create() {
+			Tilemap v = super.create();
+			v.map( new int[]{0}, 1 );
+			return v;
+		}
+
+		@Override
+		public String name(int tileX, int tileY) {
+			return Messages.get(this, "name");
+		}
+
+		@Override
+		public String desc(int tileX, int tileY) {
+			return Messages.get(this, "desc");
+		}
+
+	}
+
+	public boolean placeBlacksmith(Blacksmith blacksmith, Level level) {
+		if (hasBlacksmith) return false;
+		do {
+			blacksmith.pos = level.pointToCell(random(2));
+		} while (Actor.findChar(blacksmith.pos) != null
+				|| blacksmith.pos == level.exit()
+				|| level.heaps.get(blacksmith.pos) != null
+				|| blacksmith.pos == entrancePos);
+		level.mobs.add(blacksmith);
+		return hasBlacksmith = true;
+	}
 }
