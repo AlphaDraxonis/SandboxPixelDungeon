9e6eaa992477fe19c9f7b7897dc1042171baa1a3
AlphaDraxonis <alphadraxonis@gmail.com>
2023-08-09 22:00:09 +0200

    v0.6: changed title scene banner

--- core/src/main/java/com/alphadraxonis/sandboxpixeldungeon/scenes/TitleScene.java
+++ core/src/main/java/com/alphadraxonis/sandboxpixeldungeon/scenes/TitleScene.java
@@ -92,8 +92,8 @@ public class TitleScene extends PixelScene {
 
         align(title);
 
-        placeTorch(title.x + 22, title.y + 46);
-        placeTorch(title.x + title.width - 22, title.y + 46);
+        placeTorch(title.x + 9, title.y + 46);
+        placeTorch(title.x + title.width - 10, title.y + 46);
 
         Image signs = new Image(BannerSprites.get(BannerSprites.Type.PIXEL_DUNGEON_SIGNS)) {
             private float time = 0;
