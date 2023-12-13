ae7d412b2a282a32e2b0ad9565d42a7c2ca113e4
Evan Debenham <Evan@ShatteredPixel.com>
2023-12-13 16:43:58 -0500

    v2.3.0: adjusted magic bomb behaviour slightly: - magically conjured explosions deal physical damage, not magic damage - they still count for the friendly magic death badge however - arcane bombs and the extra effect of holy bombs do deal magic damage
    (cherry picked from commit d3d472925426a92bafdcdb1f71d90b18c955668d)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/bombs/Bomb.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/bombs/Bomb.java
@@ -212,7 +212,7 @@ public class Bomb extends Item {
 				}
 				
 				if (ch == Dungeon.hero && !ch.isAlive()) {
-					if (this instanceof MagicalBomb){
+					if (this instanceof ConjuredBomb){
 						Badges.validateDeathFromFriendlyMagic();
 					}
 					GLog.n(Messages.get(this, "ondeath"));
@@ -282,8 +282,8 @@ public class Bomb extends Item {
 		igniteOnDrop = bundle.getBoolean( IGNITE_ON_DROP );
 	}
 
-	//used to track the death from friendly magic badge
-	public static class MagicalBomb extends Bomb{};
+	//used to track the death from friendly magic badge, if an explosion was conjured by magic
+	public static class ConjuredBomb extends Bomb{};
 
 	public static class Fuse extends Actor{
 
