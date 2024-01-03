b10012b35fd0d44fa633f2f7b35250d480cad9ab
Evan Debenham <evan@shatteredpixel.com>
2024-01-03 01:41:00 +0100

    v2.3.0: fixed various cases where tutorial could become stuck

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/EntranceRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/EntranceRoom.java
@@ -35,93 +35,94 @@ import com.watabou.utils.Point;
 import com.watabou.utils.Random;
 
 public class EntranceRoom extends StandardRoom {
+	
+	@Override
+	public int minWidth() {
+		return Math.max(super.minWidth(), 5);
+	}
+	
+	@Override
+	public int minHeight() {
+		return Math.max(super.minHeight(), 5);
+	}
 
-    @Override
-    public int minWidth() {
-        return Math.max(super.minWidth(), 5);
-    }
+	@Override
+	public boolean canMerge(Level l, Point p, int mergeTerrain) {
+		return false;
+	}
 
-    @Override
-    public int minHeight() {
-        return Math.max(super.minHeight(), 5);
-    }
+	@Override
+	public boolean canPlaceTrap(Point p) {
+		if (Dungeon.getSimulatedDepth() == 1) {
+			return false;
+		} else {
+			return super.canPlaceTrap(p);
+		}
+	}
 
-    @Override
-    public boolean canMerge(Level l, Point p, int mergeTerrain) {
-        return false;
-    }
+	public void paint(Level level ) {
 
-    @Override
-    public boolean canPlaceTrap(Point p) {
-        if (Dungeon.getSimulatedDepth() == 1) {
-            return false;
-        } else {
-            return super.canPlaceTrap(p);
-        }
-    }
+		int simulatedDepth = Dungeon.getSimulatedDepth(level.levelScheme);
+		
+		Painter.fill( level, this, Terrain.WALL );
+		Painter.fill( level, this, 1, Terrain.EMPTY );
+		
+		for (Room.Door door : connected.values()) {
+			door.set( Room.Door.Type.REGULAR );
+		}
 
-    public void paint(Level level) {
+		int entrance;
+		do {
+			entrance = level.pointToCell(random(2));
+		} while (level.findMob(entrance) != null);
+		Painter.set( level, entrance, Terrain.ENTRANCE );
 
-        int simulatedDepth = Dungeon.getSimulatedDepth(level.levelScheme);
+		String dest = Dungeon.customDungeon.getFloor(Dungeon.levelName).getDefaultAbove();
+		if (Level.SURFACE.equals(dest)){
+			level.transitions.put(entrance, new LevelTransition(level, entrance, LevelTransition.Type.SURFACE));
+		} else {
+			if (Dungeon.customDungeon.getFloor(dest) != null)
+				level.transitions.put(entrance, new LevelTransition(level, entrance, LevelTransition.Type.REGULAR_ENTRANCE));
+		}
 
-        Painter.fill(level, this, Terrain.WALL);
-        Painter.fill(level, this, 1, Terrain.EMPTY);
+		//use a separate generator here so meta progression doesn't affect levelgen
+		Random.pushGenerator();
 
-        for (Room.Door door : connected.values()) {
-            door.set(Room.Door.Type.REGULAR);
-        }
+		//places the first guidebook page on floor 1
+		if (simulatedDepth == 1 &&
+				(!Document.ADVENTURERS_GUIDE.isPageRead(Document.GUIDE_INTRO) || SPDSettings.intro() )){
+			int pos;
+			do {
+				//can't be on bottom row of tiles
+				pos = level.pointToCell(new Point( Random.IntRange( left + 1, right - 1 ),
+						Random.IntRange( top + 1, bottom - 2 )));
+			} while (pos == level.entrance() || level.findMob(level.entrance()) != null);
+			level.drop( new Guidebook(), pos );
+			Document.ADVENTURERS_GUIDE.deletePage(Document.GUIDE_INTRO);
+		}
 
-        int entrance;
-        do {
-            entrance = level.pointToCell(random(2));
-        } while (level.findMob(entrance) != null);
-        Painter.set(level, entrance, Terrain.ENTRANCE);
+		//places the third guidebook page on floor 2
+		if (simulatedDepth == 2 && !Document.ADVENTURERS_GUIDE.isPageFound(Document.GUIDE_SEARCHING)){
+			int pos;
+			do {
+				//can't be on bottom row of tiles
+				pos = level.pointToCell(new Point( Random.IntRange( left + 1, right - 1 ),
+						Random.IntRange( top + 1, bottom - 2 )));
+			} while (pos == level.entrance() || level.findMob(level.entrance()) != null);
+			GuidePage p = new GuidePage();
+			p.page(Document.GUIDE_SEARCHING);
+			level.drop( p, pos );
+		}
 
-        String dest = Dungeon.customDungeon.getFloor(Dungeon.levelName).getDefaultAbove();
-        if (Level.SURFACE.equals(dest)) {
-            level.transitions.put(entrance, new LevelTransition(level, entrance, LevelTransition.Type.SURFACE));
-        } else {
-            if (Dungeon.customDungeon.getFloor(dest) != null)
-                level.transitions.put(entrance, new LevelTransition(level, entrance, LevelTransition.Type.REGULAR_ENTRANCE));
-        }
+		Random.popGenerator();
 
-        //use a separate generator here so meta progression doesn't affect levelgen
-        Random.pushGenerator();
+	}
 
-        //places the first guidebook page on floor 1
-        if (simulatedDepth == 1 &&
-                (!Document.ADVENTURERS_GUIDE.isPageRead(Document.GUIDE_INTRO) || SPDSettings.intro())) {
-            int pos;
-            do {
-                //can't be on bottom row of tiles
-                pos = level.pointToCell(new Point(Random.IntRange(left + 1, right - 1),
-                        Random.IntRange(top + 1, bottom - 2)));
-            } while (pos == level.entrance() || level.findMob(level.entrance()) != null);
-            level.drop(new Guidebook(), pos);
-        }
-
-        //places the third guidebook page on floor 2
-        if (simulatedDepth == 2 && !Document.ADVENTURERS_GUIDE.isPageFound(Document.GUIDE_SEARCHING)) {
-            int pos;
-            do {
-                //can't be on bottom row of tiles
-                pos = level.pointToCell(new Point(Random.IntRange(left + 1, right - 1),
-                        Random.IntRange(top + 1, bottom - 2)));
-            } while (pos == level.entrance() || level.findMob(level.entrance()) != null);
-            GuidePage p = new GuidePage();
-            p.page(Document.GUIDE_SEARCHING);
-            level.drop(p, pos);
-        }
-
-        Random.popGenerator();
-
-    }
-
-    @Override
-    public boolean connect(Room room) {
-        //cannot connect to exit, otherwise works normally
-        if (room instanceof ExitRoom) return false;
-        else return super.connect(room);
-    }
-
+	@Override
+	public boolean connect(Room room) {
+		//cannot connect to exit, otherwise works normally
+		if (room instanceof ExitRoom)   return false;
+		else                            return super.connect(room);
+	}
+	
 }
