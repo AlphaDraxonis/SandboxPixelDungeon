7a68c8c1e3a2506140d13d35969fe15017cd2bd4
Evan Debenham <Evan@ShatteredPixel.com>
2024-06-02 16:04:59 -0400

    v2.4.2: fixed cases where champion ability tag sprites wouldn't update
    (cherry picked from commit bee9767987b807b553f40949abe51aa31e4032bf)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/GameScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/GameScene.java
@@ -54,6 +54,7 @@ import com.shatteredpixel.shatteredpixeldungeon.items.potions.Potion;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfTeleportation;
 import com.shatteredpixel.shatteredpixeldungeon.items.trinkets.DimensionalSundial;
 import com.shatteredpixel.shatteredpixeldungeon.items.trinkets.MimicTooth;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MeleeWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.journal.Document;
 import com.shatteredpixel.shatteredpixeldungeon.journal.Journal;
 import com.shatteredpixel.shatteredpixeldungeon.levels.LastLevel;
@@ -628,6 +629,10 @@ public class GameScene extends DungeonScene {
 			updateItemDisplays = false;
 			QuickSlotButton.refresh();
 			InventoryPane.refresh();
+			if (ActionIndicator.action instanceof MeleeWeapon.Charger) {
+				//Champion weapon swap uses items, needs refreshing whenever item displays are updated
+				ActionIndicator.refresh();
+			}
 		}
 
 		if (Dungeon.hero == null || scene == null) {
