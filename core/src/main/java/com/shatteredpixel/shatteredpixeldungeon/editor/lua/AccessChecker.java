18126cd7aaaa5256dc57df4e38b97dad136e9987
AlphaDraxonis <alphadraxonis@gmail.com>
2024-07-07 20:34:49 +0200

    v1.2.0: added AccessChecker class to make certain things public that should be public in release but not in development

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/editor/lua/AccessChecker.java
@@ -0,0 +1,47 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ *
+ * Sandbox Pixel Dungeon
+ * Copyright (C) 2023-2024 AlphaDraxonis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.editor.lua;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.*;
+
+class AccessChecker {
+	//this is only to make sure these fields and methods are accessible
+	//all of them are public in release, but protected during development
+
+	//Mob.target Char.enemy
+	static {
+		new DwarfKing().getSubjects();//private
+		new Golem().canTele(0);//private
+		new Guard().chain(0);//private
+		new Succubus().blink(0);//private
+		new Thief().steal(null);//protected
+		new Mob(){}.spend(0);//protected
+		new Char(){}.throwItems();//protected
+		new Char(){}.zap();//protected
+		new Mob(){}.target = 0;//protected
+		new Mob(){}.enemy = null;//protected
+	}
+}
