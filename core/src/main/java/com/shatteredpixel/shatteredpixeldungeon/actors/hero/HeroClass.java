db8a1ee4dc36c6e27b62f0dab04452ffc791a6ac
Evan Debenham <Evan@ShatteredPixel.com>
2024-10-12 12:16:07 -0400

    v3.0.0: added WIP visuals for the cleric
    (cherry picked from commit 7ac96b40e3bdfaeea6325cffc774bfdbe638b67d)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
@@ -293,8 +293,8 @@ public enum HeroClass {
 				return Assets.Sprites.HUNTRESS;
 			case DUELIST:
 				return Assets.Sprites.DUELIST;
-			case CLERIC: //TODO CLERIC cleric sprites
-				return Assets.Sprites.HUNTRESS;
+			case CLERIC: //TODO CLERIC finish sprite sheet
+				return Assets.Sprites.CLERIC;
 		}
 	}
 
@@ -310,8 +310,8 @@ public enum HeroClass {
 				return Assets.Splashes.HUNTRESS;
 			case DUELIST:
 				return Assets.Splashes.DUELIST;
-			case CLERIC: //TODO CLERIC cleric splash
-				return Assets.Splashes.HUNTRESS;
+			case CLERIC: //TODO CLERIC finish cleric splash
+				return Assets.Splashes.CLERIC;
 		}
 	}
 	
