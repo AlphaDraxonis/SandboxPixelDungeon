a5090ea0acfabb05bc08b7f90811e61b5c4c3158
Evan Debenham <evan@shatteredpixel.com>
2025-01-22 19:25:39 +0100

    v3.0.0: reverted a retreating code optimization that caused rare errors
    Signed-off-by: AlphaDraxonis <alphadraxonis@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -1270,24 +1270,21 @@ public class Dungeon {
         return PathFinder.getStep( ch.pos, to, findPassable(ch, pass, visible, chars), ch );
 
 	}
-	
+
 	public static int flee( Char ch, int from, boolean[] pass, boolean[] visible, boolean chars ) {
 		boolean[] passable = findPassable(ch, pass, visible, false, true);
 		passable[ch.pos] = true;
 
-		//only consider other chars impassable if our retreat step may collide with them
-		if (chars) {
-			for (Char c : Actor.chars()) {
-				if (c.pos == from || Dungeon.level.adjacent(c.pos, ch.pos)) {
-					passable[c.pos] = false;
-				}
-			}
-		}
-
 		//chars affected by terror have a shorter lookahead and can't approach the fear source
 		boolean canApproachFromPos = ch.buff(Terror.class) == null && ch.buff(Dread.class) == null;
-		return PathFinder.getStepBack( ch.pos, from, canApproachFromPos ? 8 : 4, passable, canApproachFromPos, ch );
-		
+		int step = PathFinder.getStepBack( ch.pos, from, canApproachFromPos ? 8 : 4, passable, canApproachFromPos, ch );
+
+		//only consider chars impassable if our retreat step runs into them
+		while (step != -1 && Actor.findChar(step) != null && chars){
+			passable[step] = false;
+			step = PathFinder.getStepBack( ch.pos, from, canApproachFromPos ? 8 : 4, passable, canApproachFromPos, ch );
+		}
+		return step;
 	}
 
 }
