f22017159938ded973b5b5ebb6d906a79b1101a8
Evan Debenham <Evan@ShatteredPixel.com>
2025-04-03 12:25:45 -0400

    v3.1.0: fixed ally swapping not counting as movement for shurikens
    (cherry picked from commit 3b7356a4d3aabb2d00e02aa74e531037bfe3cac9)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -292,6 +292,7 @@ public abstract class Char extends Actor {
 			if (Dungeon.hero.subClass == HeroSubClass.FREERUNNER){
 				Buff.affect(Dungeon.hero, Momentum.class).gainStack();
 			}
+			Dungeon.hero.justMoved = true;
 
 			Dungeon.hero.busy();
 		}
