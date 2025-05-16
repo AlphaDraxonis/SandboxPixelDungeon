d9460feaf9e8e6b7e534531ff91479518df290a4
Evan Debenham <Evan@ShatteredPixel.com>
2025-05-16 13:17:25 -0400

    v3.1.0: change to region deco rooms, mainly to reduce sewers barrels:
    - barrel patch room spawn rate in sewers down by 50%, pipe rooms up by 33% to compensate
    - barrel patch entr/exit rooms down by 25%, bridge entr/exit up 33% to comepsnate
    - region deco patch room fill rate down to 20-30%, from 20-40%
    
    (cherry picked from commit 5bafa5395b5fe86f9b1442171e9f3dafd44d3328)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/entrance/EntranceRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/standard/entrance/EntranceRoom.java
@@ -131,8 +131,8 @@ public class EntranceRoom extends StandardRoom implements EntranceRoomInterface 
 
 	private static ArrayList<Class<?extends StandardRoom>> rooms = new ArrayList<>();
 	static {
-		rooms.add(RegionDecoPatchEntranceRoom.class);
 		rooms.add(WaterBridgeEntranceRoom.class);
+		rooms.add(RegionDecoPatchEntranceRoom.class);
 		rooms.add(RingEntranceRoom.class);
 		rooms.add(CircleBasinEntranceRoom.class);
 
