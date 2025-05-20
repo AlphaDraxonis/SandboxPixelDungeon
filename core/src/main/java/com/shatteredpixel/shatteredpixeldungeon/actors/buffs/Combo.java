9a9cc76a9f8c58d42c2291cb995760129f5a6418
Evan Debenham <Evan@ShatteredPixel.com>
2025-05-20 14:43:09 -0400

    v3.1.0: fixed a crash with lethal defense
    (cherry picked from commit 15fe48d785a4307d68460e8df846f2530305d02e)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
@@ -414,10 +414,8 @@ public class Combo extends HeroSubclassAbilityBuff {
 							ch.sprite.bloodBurstA(target.sprite.center(), aoeHit);
 							ch.sprite.flash();
 
-							if (!ch.isAlive()
-									&& hero.hasTalent(Talent.LETHAL_DEFENSE)
-									&& hero.buff(BrokenSeal.WarriorShield.class) != null) {
-								hero.buff(BrokenSeal.WarriorShield.class).reduceCooldown(hero.pointsInTalent(Talent.LETHAL_DEFENSE)/3f);
+							if (!ch.isAlive() && hero.hasTalent(Talent.LETHAL_DEFENSE)) {
+								Buff.affect(hero, BrokenSeal.WarriorShield.class).reduceCooldown(hero.pointsInTalent(Talent.LETHAL_DEFENSE)/3f);
 							}
 						}
 					}
@@ -470,7 +468,7 @@ public class Combo extends HeroSubclassAbilityBuff {
 
 		if (!enemy.isAlive() || (!wasAlly && enemy.alignment == target.alignment)) {
 			if (hero.hasTalent(Talent.LETHAL_DEFENSE)){
-				hero.buff(BrokenSeal.WarriorShield.class).reduceCooldown(hero.pointsInTalent(Talent.LETHAL_DEFENSE)/3f);
+				Buff.affect(hero, BrokenSeal.WarriorShield.class).reduceCooldown(hero.pointsInTalent(Talent.LETHAL_DEFENSE)/3f);
 			}
 		}
 
