f509a5034ba193b29a52ee92207e3cd8f2e8729b
Evan Debenham <evan@shatteredpixel.com>
2025-07-30 20:32:09 +0200

    v3.2.0: chaotic censor no longer considers passive enemies valid targets
    Signed-off-by: AlphaDraxonis <alphadraxonis@gmail.com>

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/trinkets/ChaoticCenser.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/trinkets/ChaoticCenser.java
@@ -39,6 +39,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.FlavourBuff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Regeneration;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.HeroMob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
 import com.shatteredpixel.shatteredpixeldungeon.effects.MagicMissile;
 import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
 import com.shatteredpixel.shatteredpixeldungeon.effects.TargetedCell;
@@ -109,15 +110,19 @@ public class ChaoticCenser extends Trinket {
 
 			if (left <= 0) {
 
-				if (TargetHealthIndicator.instance != null && TargetHealthIndicator.instance.isVisible()
-						&& TargetHealthIndicator.instance.target() != null
-						&& TargetHealthIndicator.instance.target().alignment == Char.Alignment.ENEMY
-						&& TargetHealthIndicator.instance.target().isAlive()) {
+				if (TargetHealthIndicator.instance != null && TargetHealthIndicator.instance.isVisible()){
+					Char target = TargetHealthIndicator.instance.target();
 
-					if (produceGas(TargetHealthIndicator.instance.target())){
-						Sample.INSTANCE.play(Assets.Sounds.GAS, 0.5f);
-						hero.interrupt();
-						left += Random.IntRange((int) (avgTurns * 0.9f), (int) (avgTurns * 1.1f));
+					if (target != null
+							&& target.isActive()
+							&& target.alignment == Char.Alignment.ENEMY
+							&& (!(target instanceof Mob) || ((Mob) target).state != ((Mob) target).PASSIVE)){
+
+						if (produceGas(target)){
+							Sample.INSTANCE.play(Assets.Sounds.GAS, 0.5f);
+							hero.interrupt();
+							left += Random.IntRange((int) (avgTurns * 0.9f), (int) (avgTurns * 1.1f));
+						}
 					}
 				}
 
