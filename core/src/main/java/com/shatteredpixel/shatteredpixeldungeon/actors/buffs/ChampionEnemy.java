f2598dbe37c3c74e3c9bca4928596ab08e558b90
Evan Debenham <evan@shatteredpixel.com>
2025-10-15 16:56:42 +0200

    v3.3.0: specific powerful earlygame enemy spawns can no longer be champs

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/ChampionEnemy.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/ChampionEnemy.java
@@ -27,8 +27,12 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Blob;
 import com.shatteredpixel.shatteredpixeldungeon.actors.blobs.Fire;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Bat;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Crab;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Guard;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mimic;
 import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Mob;
+import com.shatteredpixel.shatteredpixeldungeon.actors.mobs.Thief;
 import com.shatteredpixel.shatteredpixeldungeon.editor.levels.CustomDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
@@ -109,7 +113,15 @@ public abstract class ChampionEnemy extends Buff {
 		Class<? extends ChampionEnemy> buffCls = CLASSES[Random.Int(6)];
 
 		if (Dungeon.mobsToChampion <= 0 && Dungeon.isChallenged(Challenges.CHAMPION_ENEMIES)) {
+
+			//we block certain standout enemies on floor <10 from becoming champions
+			if (m instanceof Crab  && Dungeon.scalingDepth() <= 3) return;
+			if (m instanceof Thief && Dungeon.scalingDepth() <= 4) return;
+			if (m instanceof Guard && Dungeon.scalingDepth() <= 7) return;
+			if (m instanceof Bat   && Dungeon.scalingDepth() <= 9) return;
+
 			Buff.affect(m, buffCls);
+			Dungeon.mobsToChampion += 8;
 			if (m.state != m.PASSIVE) {
 				m.state = m.WANDERING;
 			}
