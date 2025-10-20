d7c7e1e879b7905c4faec17ba450195a3d92de7c
Evan Debenham <Evan@ShatteredPixel.com>
2025-10-20 12:40:34 -0400

    v3.3.0: Gladiators negative shield cooldown now has its own buff desc
    (cherry picked from commit be97730e01eb488980631b431558f481862e3018)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/BrokenSeal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/BrokenSeal.java
@@ -262,7 +262,7 @@ public class BrokenSeal extends Item {
 
 		@Override
 		public int icon() {
-			if (coolingDown() || shielding() > 0){
+			if (coolingDown() || shielding() > 0 || cooldown < 0){
 				return BuffIndicator.SEAL_SHIELD;
 			} else {
 				return BuffIndicator.NONE;
@@ -271,10 +271,11 @@ public class BrokenSeal extends Item {
 
 		@Override
 		public void tintIcon(Image icon) {
+			icon.resetColor();
 			if (coolingDown() && shielding() == 0){
 				icon.brightness(0.3f);
-			} else {
-				icon.resetColor();
+			} else if (cooldown < 0) {
+				icon.invert();
 			}
 		}
 
@@ -284,6 +285,8 @@ public class BrokenSeal extends Item {
 				return GameMath.gate(0, 1f - shielding()/(float)maxShield(), 1);
 			} else if (coolingDown()){
 				return GameMath.gate(0, cooldown / (float)COOLDOWN_START, 1);
+			} else if (cooldown < 0) {
+				return GameMath.gate(0, (COOLDOWN_START+cooldown) / (float)COOLDOWN_START, 1);
 			} else {
 				return 0;
 			}
@@ -293,7 +296,7 @@ public class BrokenSeal extends Item {
 		public String iconTextDisplay() {
 			if (shielding() > 0){
 				return Integer.toString(shielding());
-			} else if (coolingDown()){
+			} else if (coolingDown() || cooldown < 0){
 				return Integer.toString(cooldown);
 			} else {
 				return "";
@@ -302,8 +305,10 @@ public class BrokenSeal extends Item {
 
 		@Override
 		public String desc() {
-			if (shielding() > 0){
+			if (shielding() > 0) {
 				return Messages.get(this, "desc_active", shielding(), cooldown);
+			} else if (cooldown < 0) {
+				return Messages.get(this, "desc_negative_cooldown", cooldown);
 			} else {
 				return Messages.get(this, "desc_cooldown", cooldown);
 			}
