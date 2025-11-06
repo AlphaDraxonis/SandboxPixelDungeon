257f6f93b601316f705e35a9526d22337ac01fef
Evan Debenham <evan@shatteredpixel.com>
2025-11-06 18:54:52 +0100

    v3.3.0: adjusted random icon, added random button to hero progression

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndChooseSubclass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndChooseSubclass.java
@@ -21,7 +21,6 @@
 
 package com.shatteredpixel.shatteredpixeldungeon.windows;
 
-
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroSubClass;
@@ -30,26 +29,62 @@ import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 import com.shatteredpixel.shatteredpixeldungeon.ui.HeroIcon;
+import com.shatteredpixel.shatteredpixeldungeon.ui.IconButton;
+import com.shatteredpixel.shatteredpixeldungeon.ui.Icons;
 import com.shatteredpixel.shatteredpixeldungeon.ui.RedButton;
 import com.shatteredpixel.shatteredpixeldungeon.ui.StyledButton;
 import com.watabou.noosa.ui.Component;
+import com.watabou.utils.Random;
 
 public class WndChooseSubclass extends AbstractWndChooseSubclass {
 
+	private TitleClassComp titleClassComp;
+	
 	public WndChooseSubclass(final TengusMask tome, final Hero hero ) {
 
 		super(createTitlebar(tome),
 				Messages.get(WndChooseSubclass.class, "message"),
 				Messages.get(WndChooseSubclass.class, "cancel"),
 				hero.heroClass, tome);
+		
+		titleClassComp.random = new IconButton(Icons.SHUFFLE.get()){
+			@Override
+			protected void onClick() {
+				super.onClick();
+				GameScene.show(new WndOptions(Icons.SHUFFLE.get(),
+						Messages.get(WndChooseSubclass.class, "random_title"),
+						Messages.get(WndChooseSubclass.class, "random_sure"),
+						Messages.get(WndChooseSubclass.class, "yes"),
+						Messages.get(WndChooseSubclass.class, "no")){
+					@Override
+					protected void onSelect(int index) {
+						super.onSelect(index);
+						if (index == 0){
+							WndChooseSubclass.this.hide();
+							HeroSubClass cls = Random.oneOf(hero.heroClass.subClasses());
+							tome.choose(cls);
+							GameScene.show(new WndInfoSubclass(hero.heroClass, cls));
+						}
+					}
+				});
+			}
+			
+			@Override
+			protected String hoverText() {
+				return Messages.get(WndChooseSubclass.class, "random_title");
+			}
+		};
 
 	}
 
-	private static Component createTitlebar(TengusMask tome){
-		IconTitle titlebar = new IconTitle();
-		titlebar.icon( new ItemSprite( tome.image(), null ) );
-		titlebar.label( tome.name() );
-		return titlebar;
+	private static TitleClassComp createTitlebar(TengusMask tome){
+		TitleClassComp result = new TitleClassComp();
+		
+		result.title = new IconTitle();
+		result.title.icon( new ItemSprite( tome.image(), null ) );
+		result.title.label( tome.name() );
+		
+		return result;
 	}
 
 	@Override
@@ -76,4 +111,21 @@ public class WndChooseSubclass extends AbstractWndChooseSubclass {
 		};
 	}
 
+	private static class TitleClassComp extends Component {
+		
+		private IconTitle title;
+		private IconButton random;
+		
+		@Override
+		protected void layout() {
+			if (random == null) {
+				title.setRect(0, 0, width, 0);
+			}
+			else {
+				title.setRect(0, 0, width-16, 0);
+				random.setRect(width-16, 0, 16, 16);
+			}
+			height = title.height();
+		}
+	}
 }
