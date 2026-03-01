mr-matthias1829/SPD-Matt-Edition

v3.3.5: fixed distortion traps not possibly spawning all rare mob alts
(cherry picked from commit 59cef5cbcb8fea031121ebe4fa58147725f1e9b3)

core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
@@ -28,6 +28,7 @@ import com.watabou.utils.Random;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class MobSpawner extends Actor {
 	{
@@ -274,25 +275,32 @@ public class MobSpawner extends Actor {
 		for (int i = 0; i < rotation.size(); i++) {
 			if (Random.Float() < altChance) {
 				Class<? extends Mob> cl = rotation.get(i);
-				if (cl == Rat.class)                cl = Albino.class;
-				else if (cl == Gnoll.class)         cl = GnollExile.class;
-				else if (cl == Crab.class)          cl = HermitCrab.class;
-				else if (cl == Slime.class)         cl = CausticSlime.class;
-
-				//else if (cl == Thief.class)         cl = Bandit.class;
-                else if (cl == DM100.class)         cl = DM151.class;
-				else if (cl == Necromancer.class)   cl = SpectralNecromancer.class;
-
-				else if (cl == Brute.class)         cl = ArmoredBrute.class;
-				else if (cl == DM200.class)         cl = DM201.class;
-
-				else if (cl == Monk.class)          cl = Senior.class;
-				//chaos elemental spawning happens in Elemental.Random
-
-				else if (cl == Scorpio.class)       cl = Acidic.class;
-
-				rotation.set(i, cl);
-			}// brother im so stupid
+				Class<? extends Mob> alt = RARE_ALTS.get(cl);
+				if (alt != null) {
+					rotation.set(i, cl);
+			}
+			}
 		}
+	}
+
+	public static final HashMap<Class<?extends Mob>, Class<?extends Mob>> RARE_ALTS = new HashMap<>();
+	static {
+		RARE_ALTS.put(Rat.class,            Albino.class);
+		RARE_ALTS.put(Gnoll.class,          GnollExile.class);
+		RARE_ALTS.put(Crab.class,           HermitCrab.class);
+		RARE_ALTS.put(Slime.class,          CausticSlime.class);
+
+		//RARE_ALTS.put(Thief.class,          Bandit.class);
+		RARE_ALTS.put(Necromancer.class,    SpectralNecromancer.class);
+        RARE_ALTS.put(DM100.class,          DM151.class);
+
+		RARE_ALTS.put(Brute.class,          ArmoredBrute.class);
+		RARE_ALTS.put(DM200.class,          DM201.class);
+
+		RARE_ALTS.put(Monk.class,           Senior.class);
+		//swapping to chaos elemental actually happens in Elemental.random
+		RARE_ALTS.put(Elemental.class,      Elemental.ChaosElemental.class);
+
+		RARE_ALTS.put(Scorpio.class,        Acidic.class);
 	}
 }

AlphaDraxonis/SandboxPixelDungeon

core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
@@ -90,7 +90,7 @@ public class MobSpawner extends Actor {
 		for (ItemsWithChanceDistrComp.ItemWithCount mob : mobRotation.distrSlots) {
 			for (int i = 0; i < mob.getCount(); i++) {
 				for (Item m : mob.items) {
-					mobs.add((Mob)((MobItem) m).getObject().getCopy());
+					mobs.add(((MobItem) m).getObject().getCopy());
 				}
 			}
 		}
@@ -363,7 +363,6 @@ public class MobSpawner extends Actor {
 			// City
 			case 19:
 				if (Random.Float() < 0.025f) rotation.add(Succubus.class);
-				return;
 		}
 	}
 
@@ -373,24 +372,31 @@ public class MobSpawner extends Actor {
 		for (int i = 0; i < rotation.size(); i++) {
 			if (Random.Float() < altChance) {
 				Class<? extends Mob> cl = rotation.get(i);
-				if (cl == Rat.class)                cl = Albino.class;
-				else if (cl == Gnoll.class)         cl = GnollExile.class;
-				else if (cl == Crab.class)          cl = HermitCrab.class;
-				else if (cl == Slime.class)         cl = CausticSlime.class;
-
-				else if (cl == Thief.class)         cl = Bandit.class;
-				else if (cl == Necromancer.class)   cl = SpectralNecromancer.class;
-
-				else if (cl == Brute.class)         cl = ArmoredBrute.class;
-				else if (cl == DM200.class)         cl = DM201.class;
-
-				else if (cl == Monk.class)          cl = Senior.class;
-				//chaos elemental spawning happens in Elemental.Random
-
-				else if (cl == Scorpio.class)       cl = Acidic.class;
-
-				rotation.set(i, cl);
+				Class<? extends Mob> alt = RARE_ALTS.get(cl);
+				if (alt != null) {
+					rotation.set(i, cl);
+				}
 			}
 		}
+	}
+
+	public static final Map<Class<?extends Mob>, Class<?extends Mob>> RARE_ALTS = new HashMap<>();
+	static {
+		RARE_ALTS.put(Rat.class,            Albino.class);
+		RARE_ALTS.put(Gnoll.class,          GnollExile.class);
+		RARE_ALTS.put(Crab.class,           HermitCrab.class);
+		RARE_ALTS.put(Slime.class,          CausticSlime.class);
+
+		RARE_ALTS.put(Thief.class,          Bandit.class);
+		RARE_ALTS.put(Necromancer.class,    SpectralNecromancer.class);
+
+		RARE_ALTS.put(Brute.class,          ArmoredBrute.class);
+		RARE_ALTS.put(DM200.class,          DM201.class);
+
+		RARE_ALTS.put(Monk.class,           Senior.class);
+		//swapping to chaos elemental actually happens in Elemental.random
+		RARE_ALTS.put(Elemental.class,      Elemental.ChaosElemental.class);
+
+		RARE_ALTS.put(Scorpio.class,        Acidic.class);
 	}
 }