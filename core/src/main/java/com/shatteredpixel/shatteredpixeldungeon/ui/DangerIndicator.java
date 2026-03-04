1a5bddf42564e7adfc9e011fc2212489da5b25c4
AlphaDraxonis <alphadraxonis@gmail.com>
2026-03-04 21:43:55 +0100

    v1.4.1: fixed crash when pressing Tab in editor

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/DangerIndicator.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/DangerIndicator.java
@@ -110,7 +110,7 @@ public class DangerIndicator extends Tag {
 	@Override
 	protected void onClick() {
 		super.onClick();
-		if (Dungeon.hero.visibleEnemies() > 0) {
+		if (Dungeon.hero != null && Dungeon.hero.visibleEnemies() > 0) {
 
 			Mob target = Dungeon.hero.visibleEnemy(++enemyIndex);
 
